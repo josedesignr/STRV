efb89cd0d4b3b2bbf460517af04a662e6b28cd95
Alvaro José Solis <alvarojose827@gmail.com>
2016-08-06 16:47:53 -0500

    Menu options in mobile version, linked to site

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,6 @@ $( "#openMenu" ).click(function() {
 	$( "#nav-mobile").addClass('menu-is-open');
 });
 
-$( "#closeMenu" ).click(function() {
+$( ".closeMenu" ).click(function() {
 	$( "#nav-mobile").removeClass('menu-is-open');
 });
