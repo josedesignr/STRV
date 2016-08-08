288f300456f357b73d631a6d2a5f8536bf6c3bae
Alvaro José Solis <alvarojose827@gmail.com>
2016-08-07 22:52:10 -0500

    Testing Component injection, success.

--- app/scripts/index.jsx
+++ app/scripts/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+
+import AwesomeComponent from './AwesomeComponent.jsx';
 import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return (
+      <div>
+        <p> Hello React!</p>
+        <AwesomeComponent />
+      </div>
+    );
   }
 }
 
